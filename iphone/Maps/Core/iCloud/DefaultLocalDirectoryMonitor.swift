7e884fc29c1c37e0a44d066d4478719c6bf22e2d
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-04-15 10:33:05 +0400

    [ios] refactor: move DefaultLocalDirectoryMonitor's source and contents methods to the file manager extension
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Core/iCloud/DefaultLocalDirectoryMonitor.swift
+++ iphone/Maps/Core/iCloud/DefaultLocalDirectoryMonitor.swift
@@ -61,7 +61,7 @@ final class DefaultLocalDirectoryMonitor: LocalDirectoryMonitor {
     }
 
     do {
-      let directorySource = try DefaultLocalDirectoryMonitor.source(fileManager: fileManager, for: directory)
+      let directorySource = try fileManager.source(for: directory)
       directorySource.setEventHandler { [weak self] in
         self?.queueDidFire()
       }
@@ -96,26 +96,6 @@ final class DefaultLocalDirectoryMonitor: LocalDirectoryMonitor {
   }
 
   // MARK: - Private
-  private static func source(fileManager: FileManager, for directory: URL) throws -> DispatchSourceFileSystemObject {
-    if !fileManager.fileExists(atPath: directory.path) {
-      do {
-        try fileManager.createDirectory(at: directory, withIntermediateDirectories: true)
-      } catch {
-        throw error
-      }
-    }
-    let directoryFileDescriptor = open(directory.path, O_EVTONLY)
-    guard directoryFileDescriptor >= 0 else {
-      let errorCode = errno
-      throw NSError(domain: POSIXError.errorDomain, code: Int(errorCode), userInfo: nil)
-    }
-    let dispatchSource = DispatchSource.makeFileSystemObjectSource(fileDescriptor: directoryFileDescriptor, eventMask: [.write], queue: DispatchQueue.main)
-    dispatchSource.setCancelHandler {
-      close(directoryFileDescriptor)
-    }
-    return dispatchSource
-  }
-
   private func queueDidFire() {
     let debounceTimeInterval = 0.2
     switch state {
@@ -135,28 +115,13 @@ final class DefaultLocalDirectoryMonitor: LocalDirectoryMonitor {
     }
   }
 
-  private static func contents(fileManager: FileManager, of directory: URL, matching typeIdentifier: String, including: [URLResourceKey]) -> Set<URL> {
-    guard let rawContents = try? fileManager.contentsOfDirectory(at: directory, includingPropertiesForKeys: including, options: [.skipsHiddenFiles]) else {
-      return []
-    }
-    // Filter the contents to include only those that match the type identifier.
-    // TODO: This filtering should be removed when full directory sync including nested directories will be implemented.
-    let filteredContents = rawContents.filter { url in
-      guard let type = try? url.resourceValues(forKeys: [.typeIdentifierKey]), let urlType = type.typeIdentifier else {
-        return false
-      }
-      return urlType == typeIdentifier
-    }
-    return Set(filteredContents)
-  }
-
   private func debounceTimerDidFire() {
     guard !isPaused else { return }
     guard case .debounce(let dirSource, let timer) = state else { fatalError() }
     timer.invalidate()
     state = .started(dirSource: dirSource)
 
-    let newContents = DefaultLocalDirectoryMonitor.contents(fileManager: fileManager, of: directory, matching: fileType.typeIdentifier, including: resourceKeys)
+    let newContents = fileManager.contents(of: directory, matching: fileType.typeIdentifier, including: resourceKeys)
     let newContentMetadataItems = LocalContents(newContents.compactMap { url in
       do {
         let metadataItem = try LocalMetadataItem(fileUrl: url)
@@ -184,5 +149,43 @@ private extension DefaultLocalDirectoryMonitor.State {
     case .started: return true
     case .debounce: return true
     }
+  }
+}
+
+private extension FileManager {
+  func source(for directory: URL) throws -> DispatchSourceFileSystemObject {
+    if !fileExists(atPath: directory.path) {
+      do {
+        try createDirectory(at: directory, withIntermediateDirectories: true)
+      } catch {
+        throw error
+      }
+    }
+    let directoryFileDescriptor = open(directory.path, O_EVTONLY)
+    guard directoryFileDescriptor >= 0 else {
+      let errorCode = errno
+      throw NSError(domain: POSIXError.errorDomain, code: Int(errorCode), userInfo: nil)
+    }
+    let dispatchSource = DispatchSource.makeFileSystemObjectSource(fileDescriptor: directoryFileDescriptor, eventMask: [.write], queue: DispatchQueue.main)
+    dispatchSource.setCancelHandler {
+      close(directoryFileDescriptor)
+    }
+    return dispatchSource
+  }
+
+  // TODO: Implement an errors and exceptions handling
+  func contents(of directory: URL, matching typeIdentifier: String, including: [URLResourceKey]) -> Set<URL> {
+    guard let rawContents = try? contentsOfDirectory(at: directory, includingPropertiesForKeys: including, options: [.skipsHiddenFiles]) else {
+      return []
+    }
+    // Filter the contents to include only those that match the type identifier.
+    // TODO: This filtering should be removed when full directory sync including nested directories will be implemented.
+    let filteredContents = rawContents.filter { url in
+      guard let type = try? url.resourceValues(forKeys: [.typeIdentifierKey]), let urlType = type.typeIdentifier else {
+        return false
+      }
+      return urlType == typeIdentifier
+    }
+    return Set(filteredContents)
   }
 }
