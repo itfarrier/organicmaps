2fafc17d422a7ba7a71d8ad7fd9d5830792d41cf
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-04-15 21:26:20 +0400

    [ios] move SynchronizationError into the separate file
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Core/iCloud/SynchronizationStateManager.swift
+++ iphone/Maps/Core/iCloud/SynchronizationStateManager.swift
@@ -21,14 +21,6 @@ enum IncomingEvent {
   case didUpdateCloudContents(CloudContents)
 }
 
-@objc enum SynchronizationError: Int, Error {
-  case fileUnavailable
-  case fileNotUploadedDueToQuota
-  case ubiquityServerNotAvailable
-  case iCloudIsNotAvailable
-  case containerNotFound
-}
-
 enum OutgoingEvent {
   case createLocalItem(CloudMetadataItem)
   case updateLocalItem(CloudMetadataItem)
@@ -238,38 +230,5 @@ final class DefaultSynchronizationStateManager: SynchronizationStateManager {
 
   private static func getItemsToResolveConflicts(cloudContents: CloudContents) -> CloudContents {
     cloudContents.notTrashed.withUnresolvedConflicts(true)
-  }
-}
-
-private extension SynchronizationError {
-  static func fromError(_ error: Error) -> SynchronizationError? {
-    let nsError = error as NSError
-    switch nsError.code {
-      // NSURLUbiquitousItemDownloadingErrorKey contains an error with this code when the item has not been uploaded to iCloud by the other devices yet
-    case NSUbiquitousFileUnavailableError:
-      return .fileUnavailable
-      // NSURLUbiquitousItemUploadingErrorKey contains an error with this code when the item has not been uploaded to iCloud because it would make the account go over-quota
-    case NSUbiquitousFileNotUploadedDueToQuotaError:
-      return .fileNotUploadedDueToQuota
-      // NSURLUbiquitousItemDownloadingErrorKey and NSURLUbiquitousItemUploadingErrorKey contain an error with this code when connecting to the iCloud servers failed
-    case NSUbiquitousFileUbiquityServerNotAvailable:
-      return .ubiquityServerNotAvailable
-    default:
-      return nil
-    }
-  }
-}
-
-// MARK: - SynchronizationError + Localized Description
-extension SynchronizationError: LocalizedError {
-  var errorDescription: String? {
-    switch self {
-    case .fileUnavailable, .ubiquityServerNotAvailable:
-      return L("icloud_synchronization_error_connection_error")
-    case .fileNotUploadedDueToQuota:
-      return L("icloud_synchronization_error_quota_exceeded")
-    case .iCloudIsNotAvailable, .containerNotFound:
-      return L("icloud_synchronization_error_cloud_is_unavailable")
-    }
   }
 }
